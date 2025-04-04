abc762da13869c0ed8e0ef05d43194c499571d34
TinchoG24 <martgomez@frba.utn.edu.ar>
2025-04-03 21:38:35 -0300

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     // This is a simple Java program that prints "Hello, World!" to the console.
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        // The program uses the System.out.println method to print the message.
+        // The main method is the entry point of the program.
+        // The args parameter is an array of strings that can be used to pass command-line arguments to the program.
+
     }
 
 }
