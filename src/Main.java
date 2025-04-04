c415e58306fcc7e87e862cee4f158e56052aa9d8
TinchoG24 <martgomez@frba.utn.edu.ar>
2025-04-03 21:24:28 -0300

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    // This is a simple Java program that prints "Hello, World!" to the console.
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
